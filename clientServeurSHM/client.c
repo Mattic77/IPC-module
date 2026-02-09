ed388451e85bc2a37e12d6e413800380805688ee
Mattic77 <youneshadli09@gmail.com>
2026-02-09 14:52:55 +0100

    solution dexercice sujet 1

--- clientServeurSHM/client.c
+++ clientServeurSHM/client.c
@@ -1,30 +1,42 @@
 #include <sys/ipc.h>
-#include<sys/shm.h>
-#include<sys/stat.h>
-#include<stdio.h>
-#include<sys/wait.h>
-#include<unistd.h>
-
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define SHM 40
 
+char *tab;
+int shmid;
+
+void handler(int sig){
+    printf("\nSignal reçu, fermeture du client...\n");
+    shmdt(tab);
+    exit(EXIT_SUCCESS);
+}
+
 int main(){
-
-    char *tab;
-    int shmid;
+    signal(SIGUSR1, handler);
     
-    shmid=shmget((size_t)SHM,0,0);
-    if(shmid==-1){
+    shmid = shmget((key_t)SHM, 20, 0);
+    if(shmid == -1){
         perror("SHMGET");
-        
-    }
-    tab=(char*) shmat(shmid,NULL,0);
-    while (1)
-    {
-        printf("saisir une text");
-        scanf("%s",tab);
-        
+        exit(EXIT_FAILURE);
     }
     
-
-
+    tab = (char*) shmat(shmid, NULL, 0);
+    
+    int pid = getpid();
+    memcpy(tab, &pid, sizeof(int));
+    printf("PID %d envoyé au serveur\n", pid);
+    
+    while(1){
+        printf("Saisir un texte (max 15 caractères): ");
+        scanf("%15s", tab + sizeof(int)); 
+    }
+    
+    return 0;
 }
--- clientServeurSHM/serveur.c
+++ clientServeurSHM/serveur.c
@@ -1,32 +1,53 @@
 #include <sys/ipc.h>
-#include<sys/shm.h>
-#include<sys/stat.h>
-#include<stdio.h>
-#include<sys/wait.h>
-#include<unistd.h>
-
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define SHM 40
-#define VAL 20
-int main(){
 
-    int shmid;
-    char *tab;
+char *tab;
+int shmid;
+
+void handler(int sig){
+    printf("\n\nFermeture du serveur...\n");
     
-
-    shmid=shmget((key_t)SHM,VAL*sizeof(char),IPC_CREAT|S_IRUSR|S_IWUSR);
-    if(shmid==-1){
-        perror("SHMGET");
-        
+    int client_pid;
+    memcpy(&client_pid, tab, sizeof(int));
+    printf("PID du client récupéré: %d\n", client_pid);
+    if(client_pid > 0){
+        printf("Envoi du signal au client (PID: %d)\n", client_pid);
+        kill(client_pid, SIGUSR1);
+        sleep(1);
     }
-    tab= (char*) shmat(shmid,NULL,0);
+    
+    shmdt(tab);
+    shmctl(shmid, IPC_RMID, NULL);
+    printf("Mémoire partagée détruite\n");
+    exit(EXIT_SUCCESS);
+}
+
+int main(){
+    signal(SIGINT, handler);
+    
+    shmid = shmget((key_t)SHM, 20, IPC_CREAT | 0666);
+    if(shmid == -1){
+        perror("SHMGET");
+        exit(EXIT_FAILURE);
+    }
+    
+    tab = (char*) shmat(shmid, NULL, 0);
+    memset(tab, 0, 20);
+    
+    printf("Serveur démarré. Affichage du contenu toutes les secondes...\n");
     
     while(1){
-        for (int  i = 0; i < VAL; i++)
-        {
-            printf("tab[%d] = %s\n", i, tab[i]);
-            sleep(1);
-        }
-        
+        sleep(1);
+        printf("Contenu: %s\n", tab + sizeof(int)); 
     }
-
+    
+    return 0;
 }
